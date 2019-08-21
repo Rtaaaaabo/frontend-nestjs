7d8bc3bcb76133208b865ac804d6cc4030966b2d
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-08-21 22:07:45 +0900

    Auth Serviceの作成

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,11 +3,32 @@ import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage';
+import { User } from '../interface/user';
+import { async } from 'q';
+import { access } from 'fs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  AUTH_SERVER: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient, private storage: Storage) { }
+
+  register(userInfo: User): Observable<User> {
+    return this.http.post<User>(`${this.AUTH_SERVER}/register`, userInfo);
+  }
+
+  login(userInfo: User): Observable<any> {
+    return this.http.post(`${this.AUTH_SERVER}/login`, userInfo).pipe(
+      tap(async (res: { status: number, access_token, expires_in, user_id }) => {
+        if (res.status !== 404) {
+          await this.storage.set('ACCESS_TOKEN', res.access_token);
+          await this.storage.set('EXPIRES_IN', res.expires_in);
+          await this.storage.set('USER_ID', res.user_id);
+        }
+      })
+    );
+  }
 }
